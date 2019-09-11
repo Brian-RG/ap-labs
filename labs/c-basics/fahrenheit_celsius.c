2ba8ef566d2c69dc40e93ae0227a2549bc913b20
Brian Reyes Gálvez <brianreyesg@gmail.com>
2019-09-10 19:34:55 -0500

    fahrenheit homework
    Signed-off-by: Brian Reyes Gálvez <brianreyesg@gmail.com>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
 
 /* print Fahrenheit-Celsius table */
 
-int main()
+int main(int argc, char **argv)
 {
-    int fahr;
-
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-
-    return 0;
+    if(argc==2){
+        int fahr=atoi(argv[1]);
+        printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+        return 0;
+    }
+    if(argc==4){
+        int LOWER=atoi(argv[1]);
+        int UPPER=atoi(argv[2]);
+        int STEP=atoi(argv[3]);
+        int fahr;
+        if(STEP<=0){
+            printf("Error, please insert a value higher than 0 for the increment.\nTo make a range conversion use: ./fahrenheit_celsius <start> <end> <increment>\n");
+            exit(-1);
+        }
+        for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+	    printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+        return 0;
+    }
+    printf("Error, invalid number of arguments.\nTo convert one value use: ./fahrenheit_celsius <num_fahrenheit_degrees>\nTo make a range conversion use: ./fahrenheit_celsius <start> <end> <increment>\n");
+    exit(-1);
 }
