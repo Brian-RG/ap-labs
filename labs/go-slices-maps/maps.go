c49c3e7ffba80e6666b67d3be35c885d2632ffe1
Brian Reyes Gálvez <brianreyesg@gmail.com>
2019-09-10 23:37:06 -0500

    adding maps.go
    Signed-off-by: Brian Reyes Gálvez <brianreyesg@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	m:= make(map[string]int)
+	sp:=strings.Split(s," ")
+	for i:=0;i<len(sp);i++{
+		m[sp[i]]=m[sp[i]]+1
+	}
+	return m
+}
+
 func main() {
+	wc.Test(WordCount)
 }
