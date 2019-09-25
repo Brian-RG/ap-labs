f8d7159138c26fe2ca55effef0b08e0342858b9c
Brian Reyes Gálvez <brianreyesg@gmail.com>
2019-09-24 21:31:52 -0500

    String library
    Signed-off-by: Brian Reyes Gálvez <brianreyesg@gmail.com>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc,char** argv) {
+    if(argc!=4){
+        printf("Bad usage of program.\nCorrect use:%s <Original String> <String to add> <Substring to search>\n",argv[0]);
+        exit(-1);
+    }
+    int strlength=mystrlen(argv[1]);
+    char* newstr=mystradd(argv[1],argv[2]);
+    //originalVeryLongString Addition Add
+    printf("Original string length: %d\n",strlength);
+    printf("New string: %s\n",newstr);
+    //int found=mystrfind(newstr,argv[3]);
+    //char *f=found?"yes":"no";
+    int *found;
+    printf("SubString was found : %s\n",found=mystrfind(newstr,argv[3])?"yes":"no");
+
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,46 @@
-
-
 int mystrlen(char *str){
-    return 0;
+    int i=0;            //Initializing i as my counter
+    while(str[i]!='\0'){//Loop while i is not '\0' that is the end of every string.
+        i+=1;           //Just adding 1 to the counter
+    }
+    return i;//Return the actual size of the string
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int a=mystrlen(origin);                 //Getting the length of the original string
+    int b=mystrlen(addition);               
+    char *new=malloc((a+b)*sizeof(char));   //Allocating enough memory to store a new string containing both
+    int i=0;                                //Initialize i as my counter that will fill the new string
+    for(i;i<a;i++){                         
+        new[i]=origin[i];                   //Assigning the value of each character in the original string to the new one
+    }
+    for(int j=0;j<b;j++){
+        new[i++]=addition[j];               //Adding the additional string
+    }
+    return new;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int a=mystrlen(origin);                 //
+    int b=mystrlen(substr);
+    int i=0;
+    int val=0;
+    for(i;i<a;i++){
+        if(origin[i]==substr[0]) {
+             val=1;
+	     for(int j=1;j<b;j++){
+                if(origin[i+j]!=substr[j]){
+                    val=0;
+                    break;
+                }
+                else if(!val){
+                    val=1;
+                }
+            }
+            if(val){
+                break;
+            }
+        }
+    } 
+    return val;
 }
