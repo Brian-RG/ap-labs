10ec604a92ce457baafaff9bae64ba728b11109f
Brian Reyes Gálvez <brianreyesg@gmail.com>
2019-12-03 17:30:22 -0600

    Adding the openmp lab
    Signed-off-by: Brian Reyes Gálvez <brianreyesg@gmail.com>

--- labs/openmp/hello_par.c
+++ labs/openmp/hello_par.c
@@ -1,4 +1,11 @@
+#include "omp.h"
+#include "logger.h"
 
-int main() {
-    return 0;
+void main(){
+    #pragma omp parallel
+    {
+        int ID = omp_get_thread_num();
+        infof(" Hello %d",ID);
+        infof(" world %d \n", ID);
+    }
 }
